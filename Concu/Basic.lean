7cdb129497094f17d13c3c7a7c97d0e3a1536532
Tage Johansson <tajo9185@student.uu.se>
2023-11-20 21:26:31 +0100

    Some initial definitions of a data structure and trace.

--- Concu/Basic.lean
+++ Concu/Basic.lean
@@ -1,1 +1,15 @@
-def hello := "world"
+structure DataStructure where
+  σ : Type
+  init : σ
+  Method : Type → Type
+  guard {α} : Method α → σ → Prop
+  [decGuard {α} (mt : Method α) : DecidablePred $ guard mt]
+  cmd {α} (m : Method α) (s : σ) : guard m s → α × σ
+#check DataStructure
+
+def DataStructure.Trace (struct : DataStructure) :=
+  List ((α : Type) × struct.Method α)
+
+def DataStructure.validTrace {struct : DataStructure} (s : struct.σ) : Trace struct → Prop
+  | .nil => True
+  | (⟨_, mthd⟩ :: xs) => ∃ g : struct.guard mthd s, validTrace (struct.cmd mthd s g).2 xs

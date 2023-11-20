e1673e872e08d8de7c7c772852b192bf0d07991e
Tage Johansson <tajo9185@student.uu.se>
2023-11-20 20:32:59 +0100

    Initial commit.

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «concu» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Concu» where
+  -- add library configuration options here

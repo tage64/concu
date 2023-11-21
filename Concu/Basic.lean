2565a6dc3253a815d5f94460ced5feab9f090e46
Tage Johansson <tajo9185@student.uu.se>
2023-11-21 09:45:06 +0100

    More progress.

--- Concu/Basic.lean
+++ Concu/Basic.lean
@@ -1,15 +1,49 @@
-structure DataStructure where
+import Aesop
+import Std.Logic
+
+structure RawDataStructure where
   σ : Type
   init : σ
   Method : Type → Type
-  guard {α} : Method α → σ → Prop
-  [decGuard {α} (mt : Method α) : DecidablePred $ guard mt]
-  cmd {α} (m : Method α) (s : σ) : guard m s → α × σ
+
+structure RawDataStructure.Operation (ρ : RawDataStructure) where
+  α : Type
+  method : ρ.Method α
+  ret : α
+
+section
+open RawDataStructure
+structure DataStructure extends RawDataStructure : Type _ where
+  guard : Operation self → σ → Prop
+  [decGuard (op : Operation self) : DecidablePred $ guard op]
+  rule (op : Operation self) (s : σ) : guard op s → σ
+attribute [instance] DataStructure.decGuard
+end
 #check DataStructure
 
-def DataStructure.Trace (struct : DataStructure) :=
-  List ((α : Type) × struct.Method α)
+namespace DataStructure
 
-def DataStructure.validTrace {struct : DataStructure} (s : struct.σ) : Trace struct → Prop
+def Trace (ρ : DataStructure) :=
+  List ρ.Operation
+
+def validTrace (ρ : DataStructure) (s : ρ.σ) : ρ.Trace → Prop
   | .nil => True
-  | (⟨_, mthd⟩ :: xs) => ∃ g : struct.guard mthd s, validTrace (struct.cmd mthd s g).2 xs
+  | (op :: ops) =>
+    ∃ (g : ρ.guard op s), ρ.validTrace (ρ.rule op s g) ops
+
+private def exists_prop_decidable' {p} (P : p → Prop)
+  [Decidable p] (_ : ∀ h, Decidable (P h)) : Decidable (∃ h, P h) :=
+  exists_prop_decidable P
+    
+instance decValidTrace : Decidable (validTrace ρ s trace) := by
+  unfold validTrace
+  cases trace
+  . simp; exact inferInstance
+  . simp
+    apply exists_prop_decidable'
+    intro g
+    apply decValidTrace
+
+def Traces (ρ : DataStructure) : ρ.Trace → Prop :=
+  ρ.validTrace ρ.init
+end DataStructure
--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,8 @@ open Lake DSL
 package «concu» where
   -- add package configuration options here
 
+require aesop from git "https://github.com/JLimperg/aesop"
+
 @[default_target]
 lean_lib «Concu» where
   -- add library configuration options here

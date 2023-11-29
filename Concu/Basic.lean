fd88f63853761d032be5812f4d3f95cd09bd4d9b
Tage Johansson <tajo9185@student.uu.se>
2023-11-29 18:52:32 +0100

    Add Register and Set data structures.

--- Concu/Basic.lean
+++ Concu/Basic.lean
@@ -1,35 +1,29 @@
-import Aesop
+import Mathlib.Data.Finset.Basic
 import Std.Logic
 
-structure RawDataStructure where
+structure DataStructure : Type _ where
   σ : Type
   init : σ
   Method : Type → Type
-
-structure RawDataStructure.Operation (ρ : RawDataStructure) where
-  α : Type
-  method : ρ.Method α
-  ret : α
-
-section
-open RawDataStructure
-structure DataStructure extends RawDataStructure : Type _ where
-  guard : Operation self → σ → Prop
-  [decGuard (op : Operation self) : DecidablePred $ guard op]
-  rule (op : Operation self) (s : σ) : guard op s → σ
+  guard : Method Ret → Ret → σ → Prop
+  [decGuard (met : Method Ret) (ret) (s) : Decidable $ guard met ret s]
+  rule (met : Method Ret) (ret) (s) : guard met ret s → σ
 attribute [instance] DataStructure.decGuard
-end
-#check DataStructure
 
 namespace DataStructure
+section
+variable (ρ : DataStructure)
 
-def Trace (ρ : DataStructure) :=
+abbrev Operation :=
+  (Ret : Type) × ρ.Method Ret × Ret
+
+def Trace :=
   List ρ.Operation
 
-def validTrace (ρ : DataStructure) (s : ρ.σ) : ρ.Trace → Prop
+def validTrace (s : ρ.σ) : ρ.Trace → Prop
   | .nil => True
-  | (op :: ops) =>
-    ∃ (g : ρ.guard op s), ρ.validTrace (ρ.rule op s g) ops
+  | (⟨_, met, ret⟩ :: ops) =>
+    ∃ (g : ρ.guard met ret s), validTrace (ρ.rule met ret s g) ops
 
 private def exists_prop_decidable' {p} (P : p → Prop)
   [Decidable p] (_ : ∀ h, Decidable (P h)) : Decidable (∃ h, P h) :=
@@ -44,6 +38,77 @@ instance decValidTrace : Decidable (validTrace ρ s trace) := by
     intro g
     apply decValidTrace
 
-def Traces (ρ : DataStructure) : ρ.Trace → Prop :=
+def Traces : ρ.Trace → Prop :=
   ρ.validTrace ρ.init
+end
 end DataStructure
+
+namespace Register
+section
+variable (α : Type) [Inhabited α] [DecidableEq α]
+
+inductive Method : Type → Type
+| read : Method α
+| write : α → Method Unit
+
+def Register : DataStructure where
+  σ := α
+  init := default
+  Method := Method α
+  guard
+    | .read, ret, s => ret = s
+    | .write _, _, _ => True
+  decGuard
+    | .read, ret, s => if h : ret = s
+      then isTrue h
+      else isFalse h
+    | .write _, _, _ => isTrue trivial
+  rule
+    | .write x, _, _, _ => x
+    | .read, _, s, _ => s
+end
+end Register
+export Register (Register)
+
+namespace Set
+section
+variable (α : Type) [DecidableEq α]
+
+inductive Method : Type → Type where
+| contains : α → Method Bool
+| add : α → Method Bool
+| remove : α → Method Bool
+
+def Set : DataStructure where
+  σ := Finset α
+  init := ∅
+  Method := Method α
+  guard
+    | .contains x, true, s 
+    | .add x, false, s
+    | .remove x, true, s
+      => x ∈ s
+    | .contains x, false, s
+    | .add x, true, s
+    | .remove x, false, s
+      => x ∉ s
+  decGuard
+    | .contains _, true, _ 
+    | .add _, false, _
+    | .remove _, true, _
+    | .contains _, false, _
+    | .add _, true, _
+    | .remove _, false, _
+      => inferInstance
+  rule
+    | .add x, true, s, hIn => s.cons x hIn
+    | .remove x, true, s, _ => s.erase x
+    | .contains _, _, s, _
+    | .add _, false, s, _
+    | .remove _, false, s, _
+      => s
+end
+end Set
+export Set (Set)
+    
+
--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,8 @@ open Lake DSL
 package «concu» where
   -- add package configuration options here
 
-require aesop from git "https://github.com/JLimperg/aesop"
+--require aesop from git "https://github.com/JLimperg/aesop"
+require mathlib from git "https://github.com/leanprover-community/mathlib4"
 
 @[default_target]
 lean_lib «Concu» where
